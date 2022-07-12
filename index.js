2bfb79fc94d5fe2588359f49b2b4936c971948c0
ksj051203 <ksj054703@gmail.com>
2022-07-12 09:31:53 +0900

    test

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) =>{
   res.send('Hello World!!!!')
 })
 
+app.get('/test', (req, res) =>{
+  res.send('test')
+})
+
 app.listen(3000, () => {
   console.log(`Example app listening on port ${port}`)
 })
